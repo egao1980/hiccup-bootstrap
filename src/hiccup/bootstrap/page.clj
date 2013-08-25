125f11589f8f51fe84331baad319739f20c89428
Sam Halicke <sam@halicke.com>
2013-08-25 08:41:58 -0500

    Updated for bootstrap 3.0 Added use-bootswatch-theme

--- src/hiccup/bootstrap/page.clj
+++ src/hiccup/bootstrap/page.clj
@@ -4,10 +4,17 @@
 (defn include-bootstrap
   "Add Bootstrap CSS and JS resources to the page."
   []
-  (list 
+  (list
    (include-css "/bootstrap/css/bootstrap.css")
-   (include-css "/bootstrap/css/bootstrap-responsive.css")
+   (include-css "/bootstrap/css/bootstrap-theme.css")
    (include-js  "/bootstrap/js/bootstrap.js")))
+
+(defn use-bootswatch-theme
+  "adds an optional bootswatch theme"
+  [theme]
+  (let [theme-file (format "/bootstrap/css/%s.css" theme)]
+    (list
+     (include-css theme-file))))
 
 (defn fixed-layout
   "Encase its contents in a fixed layout container."
