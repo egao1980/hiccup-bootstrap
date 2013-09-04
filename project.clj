de6697c92972491dbfa39029e88734d0862c9791
egao1980 <egao1980@gmail.com>
2013-09-04 21:26:05 +0100

    Update project.clj

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [hiccup "1.0.3"]
+                 [hiccup "1.0.4"]
                  [compojure "1.1.5"]])
